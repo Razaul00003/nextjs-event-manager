ec502eb9dd5a558a09cfc6c70317d66a55f8098c
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-10-19 19:57:31 +0600

    second commit

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
+import Button from "../ui/button";
 function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -17,14 +21,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate} </time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{address}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore more</Link>
+          <Button link={exploreLink}>
+            <span>Explore More</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
